c0c80f1339a5b9b4ecc4e8aa76af24105d249171
Ucsus <ucsus@exbo.ru>
2024-12-14 20:26:23 +0300

    fix tests

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -21,7 +21,7 @@ class TaskTest extends TestCase
     public function testIndex()
     {
         $user = User::factory()->create();
-        $response = $this->actingAs($user)->get(route('tasks.index'));
+        $response = $this->withSession(['banned' => false])->get(route('/tasks'));
         $response->assertOk();
     }
 
